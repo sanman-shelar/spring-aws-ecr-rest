2c6e83da202a41f972424de9c74c07448a8cb803
Sanman Shelar <39533009+sanman-shelar@users.noreply.github.com>
2020-12-19 18:28:18 +0530

    updated config to trigger beanstalk app deployment

--- src/main/java/com/aws/HelloController.java
+++ src/main/java/com/aws/HelloController.java
@@ -11,4 +11,9 @@ public class HelloController {
     public String hello(@PathVariable String name) {
         return "Hello ".concat(name).concat("!");
     }
+    
+    @GetMapping("/bye/{name}")
+    public String bye(@PathVariable String name) {
+        return "Bye ".concat(name).concat("!");
+    }
 }
